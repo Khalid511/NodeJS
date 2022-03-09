6f9ff9f49478219de807cbf840ded7bc5e338d11
Md. Khalid Hasan <shohag9438@gmail.com>
2022-03-09 15:08:43 +0600

    Assignment-1, Task-3, Express router for the /leaders and /leaders/:leaderId REST API end points.

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const hostname = 'localhost', port = 3000;
 const app = express();
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 app.use('/dishes', dishRouter);
 app.use('/dishes/:dishId', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/promotions/:promoId', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderId', leaderRouter);
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
