d49c43aae8ac0d13085144b9fbd84566f0a347bc
Md. Khalid Hasan <shohag9438@gmail.com>
2022-03-09 15:02:47 +0600

    Assignment-1, Task-2, Express router for the /promotions and /promotions/:promoId REST API end points.

--- index.js
+++ index.js
@@ -7,32 +7,15 @@ const hostname = 'localhost', port = 3000;
 
 const app = express();
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
 
 app.use('/dishes', dishRouter);
 app.use('/dishes/:dishId', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-
-
-
-// app.get('/dishes/:dishId', (req, res, next) => {
-//     res.end('Will send the details of the dish: ' +req.params.dishId +' to you.');
-// });
-
-// app.post('/dishes/:dishId', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end('POST operation not supported on /dishes/'+req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req, res, next) => {
-//     res.write('Updating the dish: ' +req.params.dishId+'\n');
-//     res.end('will update the dish: '+req.body.name +' with details: '+req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end('Deleting dish: '+req.params.dishId);
-// });
 
 
 
